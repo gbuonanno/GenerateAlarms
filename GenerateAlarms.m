6cdb8962e1bfffff5d2653d9571b8493951b31aa
auduser <auduser@DESKTOP-FFSB84P.mshome.net>
2022-05-11 13:39:46 +0200

    First connection to the MySQL database

--- GenerateAlarms.m
+++ GenerateAlarms.m
@@ -7,5 +7,40 @@ clc
 
 j=8;
 
+% Establish connection with MySQL database 
+vendor = "MySQL";
+opts = databaseConnectionOptions("jdbc",vendor);
+opts = setoptions(opts, ...
+    'DataSourceName',"MySQL", ...
+    'JDBCDriverLocation',"C:\Program Files (x86)\MySQL\Connector J 8.0\mysql-connector-java-8.0.28.jar", ...
+    'DatabaseName',"test",'Server',"localhost", ...
+    'PortNumber',3306);
+
+% Establish connection the local host, username and password
+username = "root";
+password = "SUNRISE";
+status = testConnection(opts,username,password);
+saveAsDataSource(opts)
+
+datasource = "MySQL";
+conn = database(datasource,username,password);
+%In case of failed connection message in command window
+
+conn.Message
+
+table_name = "test.alarms";
+%(Primary key was eliminated for testing )
+ID=2;
+Unique_ID=2;
+Date= "2022-10-03 11:15:01";
+
+Data = table(ID,Unique_ID,Date,'VariableNames',{'ID','Unique_ID','Date'});
+sqlwrite(conn,table_name,Data);
+
+
+
+
+
+
 
 
